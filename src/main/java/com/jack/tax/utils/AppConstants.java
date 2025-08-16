38106c4645ab35c97a8aaf940e7870bacc77f42c
jwhougland <47694635+jwhougland@users.noreply.github.com>
2025-08-16 16:29:45 -0500

    Added validation for tax calculation API input

---
+++ src/main/java/com/jack/tax/utils/AppConstants.java
@@ -0,0 +1,17 @@
+package com.jack.tax.utils;
+
+/**
+ * Application constants
+ */
+public class AppConstants {
+
+    /**
+     * Min USD field amount as a string
+     */
+    public static final String MIN_USD_AMOUNT_AS_STR = "0.0";
+
+    /**
+     * Max USD field amount as a string
+     */
+    public static final String MAX_USD_AMOUNT_AS_STR = "2147483647.0";
+}
